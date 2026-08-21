d2e08a8c54607a783a00c5537ebd2d1c69017e75
jadersouza-CREATOR <jader.souza@academico.unirv.edu.br>
2026-08-21 13:59:53 -0300

    Ativar integração de pagamentos Pix junto da extensão de vendas pendentes

--- sitecustomize.py
+++ sitecustomize.py
@@ -2,22 +2,20 @@ import importlib.abc
 import importlib.machinery
 import sys
 
-
 class _AppLoader(importlib.abc.Loader):
     def __init__(self, spec):
         self.spec = spec
         self.loader = spec.loader
-
     def create_module(self, spec):
         if hasattr(self.loader, "create_module"):
             return self.loader.create_module(spec)
         return None
-
     def exec_module(self, module):
         self.loader.exec_module(module)
-        from pending_extension import instalar
-        instalar(module.app)
-
+        from pending_extension import instalar as instalar_pendentes
+        instalar_pendentes(module.app)
+        from payment_extension import instalar as instalar_pagamentos
+        instalar_pagamentos(module.app)
 
 class _AppFinder(importlib.abc.MetaPathFinder):
     def find_spec(self, fullname, path=None, target=None):
@@ -28,7 +26,6 @@ class _AppFinder(importlib.abc.MetaPathFinder):
             spec.loader = _AppLoader(spec)
             return spec
         return None
-
 
 if not any(isinstance(finder, _AppFinder) for finder in sys.meta_path):
     sys.meta_path.insert(0, _AppFinder())
