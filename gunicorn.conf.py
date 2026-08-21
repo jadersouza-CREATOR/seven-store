ad5a4a86273b8849ebd894968cc333d1d91d5da5
jadersouza-CREATOR <jader.souza@academico.unirv.edu.br>
2026-08-21 14:12:35 -0300

    Ativar confirmação automática de pagamentos Pix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,6 @@
 def post_worker_init(worker):
     import app as modulo
-    from atendimento_extension import instalar
-    instalar(modulo)
+    from atendimento_extension import instalar as instalar_atendimento
+    instalar_atendimento(modulo)
+    from pagamento_extension import instalar as instalar_pagamento
+    instalar_pagamento(modulo)
